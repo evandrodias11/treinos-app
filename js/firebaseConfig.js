337d8e569c86542c8845bcbfb95e04646c2eb9e7
evandrodias11 <evandro.dias@paripassu.com.br>
2024-09-16 08:41:32 -0300

    add ignore

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,15 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
-
-// Configuração do Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyDvm622azP1rA3U0fhyi9_G4cncmQGcD4U",
-  authDomain: "treinos-app-17791.firebaseapp.com",
-  projectId: "treinos-app-17791",
-  storageBucket: "treinos-app-17791.appspot.com",
-  messagingSenderId: "29487857882",
-  appId: "1:29487857882:web:e5f688bf173b2f3c98a5e3",
-};
+import { firebaseConfig } from "../js/config.js";
 
 // Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
